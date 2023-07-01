aceda449b286d19491dfe9d241cc7dbf1b99428a
Theo Breaux <theobreaux@gmail.com>
2023-06-30 18:04:48 -0700

    using context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,14 +2,14 @@ import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>5755</span>
+      <span className={classes.badge}>{}</span>
     </button>
   );
 };
